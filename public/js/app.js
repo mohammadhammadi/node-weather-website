4ff5c777faf5f2fd5c64418dfa363067845488d8
moehammadi <hammadimohammad@gmail.com>
2019-09-04 13:58:54 +0300

    Updated Forecast Visible Data

--- public/js/app.js
+++ public/js/app.js
@@ -3,25 +3,22 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-weatherForm.addEventListener('submit',(e) => {
-    e.preventDefault();
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    const location = search.value
-    
     fetch('/weather?address=' + location).then((response) => {
-    response.json().then((data) => {
-
-        if(data.error)
-        {
-            messageOne.textContent = data.error
-            return
-        }
-        messageOne.textContent = data.location + data.forecastData.timezone
-        messageTwo.textContent = data.forecastData.summary + 'It is currently ' + data.forecastData.temperature + ' degrees out. There is a ' + data.forecastData.sprecip + '% chance of rain.'
-        
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
     })
-})
 })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,25 +1,16 @@
 const request = require('request')
 
-const forecast = (latitude,longitude,callback) => {
-    const url = 'https://api.darksky.net/forecast/ab07f66cb4f834a0d0a331f454e49f9e/' + latitude + ',' + longitude + '?units=si'
-    request({ url, json: true }, (error, {body}) => {
-        
-        if (error) {
-            callback('Unable to connect to weather service!',undefined)
-            return
-        }
-        
-        if (body.error) {
-            callback('Unable to find location',undefined)
-            return
-        }
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/ab07f66cb4f834a0d0a331f454e49f9e/' + latitude + ',' + longitude
 
-        callback(undefined,{
-            summary: body.daily.data[0].summary,
-            temperature: body.currently.temperature,
-            sprecip: body.currently.precipProbability,
-            timezone: body.timezone
-        })
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
     })
 }
 
